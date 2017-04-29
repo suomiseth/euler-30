a4eb24aee9d1adc3be710f9e1711f343add2e035
suomiseth <suomiseth@gmail.com>
2017-04-29 11:18:04 -0400

    working, but slow algo

--- brute-force.rb
+++ brute-force.rb
@@ -2,38 +2,33 @@ require "pry-byebug"
 
 class SumOfFifthPowerDigits
 
-  def initialize(num_of_digits, max_num)
-    @digits = num_of_digits
+  def initialize(power, max_num)
+    @power = power
     @max_num = max_num
     @solution_set = []
   end
 
-  def iterate_down
-    (1..@max_num).map do |num|
-      if equal_sums?(num)
-        @solution_set << num
-      end
+  def iterate_through
+    (2..@max_num).map do |num|
+      @solution_set << num if equal_sums?(num)
     end
   end
 
   def equal_sums?(num)
-    num.to_s.split("").map do |dig| 
-      sum = []
-      if dig.to_i > 1 ? sum << dig.to_i ** @digits 
-      return true if (sum.inject(:+) == num)
-    end
+    numbers = num.to_s.split("").map {|dig| dig.to_i ** @power}
+    (numbers.inject(:+) == num) ? true : false
   end
 
   def answer
-    iterate_down
-    @solution_set
+    iterate_through
+    binding.pry
+    @solution_set.inject(:+)
   end
-
 end
 
 
 puts "What power do you want to do?"
-ints = gets.strip.to_i
+power = gets.strip.to_i
 puts "whats the biggest number you want to look at?"
 max_num = gets.strip.to_i
-puts "#{SumOfFifthPowerDigits.new(ints, max_num).answer}"
+puts "#{SumOfFifthPowerDigits.new(power, max_num).answer}"
